c4c7b41fcb12abfd7dfff8874dcbcb35946e250a
mitchless <mitch.oliver@gmail.com>
2013-06-27 12:03:13 -0400

    Add some comments to explain what's going on

--- apps/tradewinds/states/abroholos_state.js
+++ apps/tradewinds/states/abroholos_state.js
@@ -2,6 +2,8 @@ sc_require('states/abroholos_internal_state');
 
 Tradewinds.AbroholosState = SC.State.extend({
 
+  // Properties preceeded by '_' are internal to this object; in an object oriented environment they
+  // would be "private."
   _abroholosView: null,
   _abroholosController: null,
 
@@ -15,6 +17,8 @@ Tradewinds.AbroholosState = SC.State.extend({
     }));
 
     // create mediating controllers
+    // Notice that I'm using "set" here. That is because I'm going to bind to _abroholosController
+    // later on in a substate.
     this.set('_abroholosController',
       SC.ObjectController.create({
         content: Tradewinds.store.materializeRecord(key)
@@ -24,6 +28,9 @@ Tradewinds.AbroholosState = SC.State.extend({
     // TODO: create view controllers
 
     // create view
+    // Here I'm not using set. Because I will not be binding to _abroholosView it is not necessary.
+    // However, it may be desirable to enforce uniform access and require that all properties be
+    // accessed via get/set.
     this._abroholosView = Tradewinds.AbroholosPane.create({
       defaultResponder: this.get('statechart')//,
     });
@@ -69,6 +76,11 @@ Tradewinds.AbroholosState = SC.State.extend({
 
   // This state demonstrates how to have data that is owned by one state used by a child state while
   // avoiding tight coupling.
+  // Here I'm binding to a controller owned by the parent state. Be aware of the context: within the
+  // "plugin" call, .parentState refers to the instance Tradewinds.AbroholosState.
+  // In my opinion, this does not violate the internal visibility of _abroholosController because
+  // this instance of Tradewinds.AbroholosInternalState (named internal) is itself internal to
+  // Tradewinds.AbroholosState
   internal: SC.State.plugin('Tradewinds.AbroholosInternalState', {
     superControllerBinding: SC.Binding.oneWay('.parentState._abroholosController')
   })
